da7ada3e1a0c2afe644122c42ce9994624e10bc7
Jeffesson Gomes <jeffessong4@gmail.com>
2022-12-28 19:30:19 -0300

    feature(Routes): creating application routes using react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import styles from './Global.module.css'
 
-import Header from "./assets/components/Header"
+import Header from './assets/components/Header';
+
+import { Home } from './pages/Home';
+import { List } from './pages/List';
+
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <div className={styles.container} >
-    
-    <Header />
-  </div>
+  return (
+      <div className={styles.container} >
+        <Header />
+        <Routes>
+          <Route index element={<Home />}/>
+          <Route path="list" element={<List />} />
+        </Routes>
+      </div>
+    )
 }
 
 export default App
